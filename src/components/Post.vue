29a0bf584042890843c579372ed88f4637ef376c
minenicole <minenicole@gmail.com>
2019-03-05 10:18:10 +0900

    first commit

---
+++ src/components/Post.vue
@@ -0,0 +1,14 @@
+<template>
+  <div :class="['logo', {selected} ? 'logo--absolute': '', {ready} ? 'logo--active' : '']" @click="closePost()">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Post'
+}
+</script>
+
+<style lang="sass">
+  @import ../assets/styles/style
+</style>
